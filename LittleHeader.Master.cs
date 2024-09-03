2907e5aeef8c2235c9bc595950affc7c0c3dc1ef
Vinicius Pavani <125597236+ViniciusPavani7@users.noreply.github.com>
2024-09-03 15:14:04 -0300

    Add files via upload

---
+++ LittleHeader.Master.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _2024_08_22_TuneRate
+{
+    public partial class LittleHeader : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SearchBox_TextChanged(object sender, EventArgs e)
+        {
+            string query = SearchBox.Text;
+
+            Response.Redirect($"~/Resultados.aspx?search={query}");
+        }
+    }
+}
---
+++ MainPage.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/LittleHeader.Master" AutoEventWireup="true" CodeBehind="MainPage.aspx.cs" Inherits="_2024_08_22_TuneRate.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
